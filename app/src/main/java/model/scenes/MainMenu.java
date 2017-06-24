70957a2838d0e8dd95867de4c4873211a51acc4f
Вадим Гатауллин <Vadya@MacBook-Pro-Vadim.local>
2017-06-24 15:22:31 +0300

    some refactorings

--- app/src/main/java/model/scenes/MainMenu.java
+++ app/src/main/java/model/scenes/MainMenu.java
@@ -1,9 +1,35 @@
 package model.scenes;
 
+import android.graphics.Canvas;
+
 /**
  * Created by Vadya on 24.06.17.
  */
 
 public final class MainMenu extends Scene {
 
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void justTap(float x, float y) {
+
+    }
+
+    @Override
+    public void movedTouch(float x1, float y1, float x2, float y2) {
+
+    }
 }
