f1a20e294b42d977af9e2454b395fab4f0fd4197
Vadya44 <44vadimko@gmail.com>
2017-07-22 02:09:43 +0500

    continue of develop

--- app/src/main/java/model/GameField.java
+++ app/src/main/java/model/GameField.java
@@ -1,12 +1,14 @@
 package model;
 
 import android.graphics.*;
+import java.util.*;
 
 /**
  * Created by Vadya on 02.07.17.
  */
 // TODO: Need to develop classes for GameField and himself
 public final class GameField extends DrawableElement {
+
     @Override
     public void onDraw(Canvas canvas) {
 
--- app/src/main/java/view/GameProcess.java
+++ app/src/main/java/view/GameProcess.java
@@ -1,5 +1,8 @@
 package view;
 
+import model.Circle;
+import model.Line;
+import model.Point;
 import model.scenes.Scene;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -9,6 +12,8 @@ import android.view.animation.Animation;
 import android.view.animation.DecelerateInterpolator;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
 
 import model.DrawableElement;
 import model.buttons.Button;
@@ -19,11 +24,28 @@ import vadyaproduction.sim.GameView;
  */
 
 public final class GameProcess extends Scene {
+
+    private int _ind1, _ind2, _ind3;
+    private boolean _endGameFlag = false;
+    private boolean _blockFlag = false;
+    public Timer aTimer = new Timer();
+    private ArrayList<Line> _plLines;
+    private ArrayList<Line> _botLines;
+    private ArrayList<Line> _usedLines;
+    private ArrayList<Point> _points;
     public static GameProcess instance;
     private int counter;
+
+
     public GameProcess(GameView view, int counter)
     {
         this.view = view;
         this.counter = counter;
+        _plLines = new ArrayList<Line>();
+        _botLines = new ArrayList<Line>();
+        _usedLines = new ArrayList<Line>();
+        _points = new ArrayList<Point>();
+        Circle circle = new Circle();
+        _points = 
     }
 }
